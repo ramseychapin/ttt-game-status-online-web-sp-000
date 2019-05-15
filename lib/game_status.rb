5ccc68af9fac47ec967bd68da3ecd192d9625bba
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-15 00:35:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,27 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |winning_combination|
+    if (board[winning_combination[0]] == "X" && board[winning_combination[1]] == "X" && board[winning_combination[2]] == "X" || board[winning_combination[0]] == "O" && board[winning_combination[1]] == "O" && board[winning_combination[2]] == "O")
+      return winning_combination
+    end
+  end
+  return false
+end
+
+
+#win_index_1 = winning_combination[0]
+#win_index_2 = winning_combination[1]
+#win_index_3 = winning_combination[2]
